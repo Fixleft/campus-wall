1cebdab46b17af72ecd2c725fd28a15c2c795040
ChaoSAMA <554640824@qq.com>
2025-11-26 10:55:06 +0800

    Initial commit for campus-wall project

---
+++ backend/src/main/java/com/campuswall/CampusWallApplication.java
@@ -0,0 +1,13 @@
+package com.campuswall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusWallApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/campuswall/campuswall/CampusWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuswall.campuswall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusWallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
